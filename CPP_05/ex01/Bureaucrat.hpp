8720aa67cf36502e506ab57562e5c5f436ee5d3e
Daniela Alexandra Reis Matos <dreis-ma@student.42porto.com>
2023-12-09 16:22:14 +0000

    C++ Repetition and Exceptions: ex01

--- CPP_05/ex01/Bureaucrat.hpp
+++ CPP_05/ex01/Bureaucrat.hpp
@@ -17,13 +17,6 @@
 #include <exception>
 #include "Form.hpp"
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define CYAN     "\033[36m"
-#define MAGENTA  "\033[35m"
-
 class Form;
 
 class Bureaucrat {
