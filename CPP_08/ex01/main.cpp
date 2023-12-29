885188a0bc0e27984ba3dda28eea996d32f5961e
Daniela Matos <danielareismatos@gmail.com>
2023-12-29 20:54:19 +0000

    C++ 08 and 09: initial set up

---
+++ CPP_08/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dreis-ma <dreis-ma@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 15:53:38 by dreis-ma          #+#    #+#             */
+/*   Updated: 2023/12/03 16:07:51 by dreis-ma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main()
+{
+    Span sp = Span(5);
+
+    sp.addNumber(6);
+    sp.addNumber(3);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
+
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+
+    return 0
+}
