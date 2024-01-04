338f10b025c8febbfc2e2979e2f00e9d53ceca06
Daniela Matos <danielareismatos@gmail.com>
2024-01-04 22:10:36 +0000

    C++ Templated containers, iterators, algorithms: ex02

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,21 +6,14 @@
 
 template <typename T>
 class MutantStack : public std::stack<T> {
-private:
-
 public:
-    MutantStack() : std::stack<T>() {};
-  //  MutantStack(const MutantStack &other) {};
-  //  MutantStack &operator=(const MutantStack &other) {};
-    ~MutantStack() {};
+    MutantStack();
+    MutantStack(const MutantStack &other);
+    MutantStack& operator=(const MutantStack &other);
+    ~MutantStack();
 
-    // Iterator typedefs
     typedef typename std::stack<T>::container_type::iterator iterator;
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-    // Iterator functions
-    iterator begin() { return this->c.begin(); }
-    iterator end() { return this->c.end(); }
-    const_iterator begin() const { return this->c.begin(); }
-    const_iterator end() const { return this->c.end(); }
+    iterator begin();
+    iterator end();
 };
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include "MutantStack.tpp"
+#include <list>
 
 int main()
 {
@@ -19,27 +21,56 @@ int main()
     mstack.push(5);
     mstack.push(17);
 
-    std::cout << mstack.top() << std::endl;
+    std::cout << "Top: " << mstack.top() << std::endl;
 
     mstack.pop();
 
-    std::cout << mstack.size() << std::endl;
+    std::cout << "Size: " << mstack.size() << std::endl;
 
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
 
     ++it;
     --it;
+    std::cout << "Elements: " << std::endl;
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
-    std::stack<int> s(mstack);
+    std::cout << std::endl;
+
+    // Testing with std::list
+    std::list<int> l;
+    l.push_back(5);
+    l.push_back(17);
+
+    std::cout << "Back: " << l.back() << std::endl;
+
+    l.pop_back();
+
+    std::cout << "Size: " << l.size() << std::endl;
+
+    l.push_back(3);
+    l.push_back(5);
+    l.push_back(737);
+    l.push_back(0);
+
+    std::list<int>::iterator lit = l.begin();
+    std::list<int>::iterator lite = l.end();
+
+    ++lit;
+    --lit;
+
+    std::cout << "Elements: " << std::endl;
+    while (lit != lite) {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
     return 0;
 }
