77f6d7ebc5303dc61a00eb8b590c98e9f6166e91
Daniela Matos <danielareismatos@gmail.com>
2024-01-13 17:18:21 +0000

    C++ casts: Conversion of scalar types

--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -10,8 +10,8 @@ class ScalarConverter
 {
 private:
     ScalarConverter();
-    ScalarConverter(const ScalarConverter &other);
-    ScalarConverter &operator=(const ScalarConverter &other);
+    ScalarConverter(const ScalarConverter&);
+    ScalarConverter &operator=(const ScalarConverter&);
     ~ScalarConverter();
 
 public:
