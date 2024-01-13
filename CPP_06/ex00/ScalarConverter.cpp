bdc153a7abe08b21886976cd94cf4d04c6dc1ada
Daniela Matos <danielareismatos@gmail.com>
2024-01-13 17:09:07 +0000

    C++ casts: Conversion of scalar types

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -1,18 +1,12 @@
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter()
-{
-    std::cout << "ScalarConverter created" << std::endl;
+ScalarConverter::ScalarConverter() {
 }
 
-ScalarConverter::ScalarConverter(const ScalarConverter&)
-{
-    std::cout << "ScalarConverter created (Copy constructor)" << std::endl;
+ScalarConverter::ScalarConverter(const ScalarConverter&) {
 }
 
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter &)
-{
-    std::cout << "ScalarConverter created (Copy assignment)" << std::endl;
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter &) {
     return (*this);
 }
 
@@ -21,104 +15,167 @@ ScalarConverter::~ScalarConverter() {
 
 // -------------------------- //
 
+static void printOutput(char* c, int* i, float* f, double* d)
+{
+    // Char
+    if (c == NULL)
+        std::cout << "char: impossible" << std::endl;
+    else if (!isprint(*c))
+        std::cout << "char: Non displayable" << std::endl;
+    else
+        std::cout << "char: '" << *c << "'" << std::endl;
+
+    // Int
+    if (i == NULL)
+        std::cout << "int: impossible" << std::endl;
+    else
+        std::cout << "int: " << *i << std::endl;
+
+    // Float
+    if (f == NULL)
+        std::cout << "float: impossible" << std::endl;
+    else
+    {
+        std::stringstream str_f;
+        str_f << *f;
+        if (*f != std::numeric_limits<float>::infinity() &&
+            *f != std::numeric_limits<float>::quiet_NaN() &&
+            int(*f * 10) % 10 == 0)
+            str_f << ".0";
+        std::cout << "float: " << str_f.str() << "f" << std::endl;
+    }
+
+    // Double
+    if (d == NULL)
+        std::cout << "double: impossible" << std::endl;
+    else
+    {
+        std::stringstream str_d;
+        str_d << *d;
+        if (*d != std::numeric_limits<float>::infinity() &&
+            *d != std::numeric_limits<float>::quiet_NaN() &&
+            int(*d * 10) % 10 == 0)
+            str_d << ".0";
+        std::cout << "double: " << str_d.str() << std::endl;
+    }
+}
+
+static void convertFromDouble(const double input)
+{
+    char c;
+    int i;
+
+    char *pC = NULL;
+    int *pI = NULL;
+
+    if (!(input > INT_MAX || input < INT_MIN || std::isnan(input)))
+    {
+        c = static_cast<char>(input);
+        i = static_cast<int>(input);
+
+        pC = &c;
+        pI = &i;
+    }
+
+    float f = static_cast<float>(input);
+    double d = input;
+
+    printOutput(pC, pI, &f, &d);
+}
+
+static void convertFromFloat(const float input)
+{
+    char c;
+    int i;
+
+    char *pC = NULL;
+    int *pI = NULL;
+
+    if (!(input > INT_MAX || input < INT_MIN || std::isnan(input)))
+    {
+        c = static_cast<char>(input);
+        i = static_cast<int>(input);
+
+        pC = &c;
+        pI = &i;
+    }
+
+    float f = input;
+    double d = static_cast<double>(input);
+
+    printOutput(pC, pI, &f, &d);
+}
+
+static void convertFromInt(const int input)
+{
+    char c = static_cast<char>(input);
+    int i = input;
+    float f = static_cast<float>(input);
+    double d = static_cast<double>(input);
+    printOutput(&c, &i, &f, &d);
+}
+
+static void convertFromChar(const char input)
+{
+    char c = input;
+    int i = static_cast<int>(input);
+    float f = static_cast<float>(input);
+    double d = static_cast<double>(input);
+    printOutput(&c, &i, &f, &d);
+}
+
+static bool isValidString(const std::string& input, const std::string &allowedChars)
+{
+    for (std::string::const_iterator it = input.begin(); it != input.end(); ++it) {
+        if (allowedChars.find(*it) == std::string::npos) {
+            return (false);
+        }
+    }
+    return (true);
+}
+
+static void convertToActualType(const std::string& input)
+{
+    if (input.size() == 1 && !isdigit(input[0]))
+    {
+        convertFromChar(input[0]);
+        return ;
+    }
+
+    if (isValidString(input, "0123456789+-"))
+    {
+        try {
+            convertFromInt(std::stoi(input));
+            return ;
+        }
+        catch (const std::out_of_range& e) {
+        }
+    }
+
+    if (input == "-inff" || input == "+inff" || input == "nanf" || (isValidString(input, "0123456789.f+-") && (input.find('f') == input.size() - 1)))
+    {
+        try {
+            convertFromFloat(std::stof(input));
+            return ;
+        }
+        catch (const std::out_of_range& e) {
+        }
+    }
+
+    if (input == "-inf" || input == "+inf" || input == "nan" || isValidString(input, "0123456789.+-"))
+    {
+        try {
+            convertFromDouble(std::stod(input));
+            return ;
+        }
+        catch (const std::out_of_range& e) {
+        }
+    }
+
+    printOutput(NULL, NULL, NULL, NULL);
+}
 
 void ScalarConverter::convert(const std::string& input)
 {
-    // Convert to char
-    // If float 42.0f has to print '*'
-    if (input == "nan")
-        std::cout << "char: impossible" << std::endl;
-    else if (input.length() == 1 && std::isprint(input[0]) && isalpha(input[0])) {
-        char charValue = input[0];
-        std::cout << "char: '" << charValue << "'" << std::endl;
-    } else {
-        std::cout << "char: Non displayable" << std::endl;
-    }
-
-    // Convert to int
-   /* try {
-        int intValue = std::stoi(input);
-        std::cout << "int: " << intValue << std::endl;
-    } catch (const std::invalid_argument& e) {
-        std::cout << "int: impossible" << std::endl;
-    } catch (const std::out_of_range& e) {
-        std::cout << "int: impossible" << std::endl;
-    }*/
-    if (input == "nan")
-        std::cout << "int: impossible" << std::endl;
-    else
-    {
-        std::stringstream ss(input);
-        int intValue;
-        if (!(ss >> intValue))
-        {
-            if (ss.str().length() == 1)
-            {
-                intValue = static_cast<int>(ss.str()[0]);
-                std::cout << "int: " << intValue << std::endl;
-            }
-
-            else
-                std::cout << "int: impossible" << std::endl;
-        }
-        else
-            std::cout << "int: " << intValue << std::endl;
-       /* try {
-            size_t pos;
-            int intValue = std::stoi(input, &pos);
-            if (pos == input.length()) {
-                std::cout << "int: " << intValue << std::endl;
-            } else {
-                throw std::invalid_argument("int: impossible");
-            }
-        } catch (const std::invalid_argument& e) {
-            std::cout << "int: impossible" << std::endl;
-        }*/
-    }
-
-
-    // Convert to float
-    try {
-        size_t pos;
-        float floatValue;
-
-        // Attempt to convert without the 'f' suffix
-        floatValue = std::stof(input, &pos);
-
-        // If successful, check if the remaining characters are valid
-        if (pos == input.length()) {
-            std::cout << "float: " << std::fixed << std::setprecision(1) << floatValue << "f" << std::endl;
-            return;
-        }
-    } catch (const std::invalid_argument& e) {
-        // Ignore and proceed to the next attempt
-    }
-
-    // If the conversion without 'f' fails, attempt to convert with 'f' removed
-    try {
-        size_t pos;
-        float floatValue = std::stof(input.substr(0, input.length() - 1), &pos);
-        if (pos == input.length() - 1) {
-            std::cout << "float: " << std::fixed << std::setprecision(1) << floatValue << "f" << std::endl;
-        } else {
-            throw std::invalid_argument("Invalid conversion");
-        }
-    } catch (const std::invalid_argument& e) {
-        std::cout << "float: impossible" << std::endl;
-    }
-
-
-    // Convert to double
-    try {
-        size_t pos;
-        double doubleValue = std::stod(input, &pos);
-        if (pos == input.length()) {
-            std::cout << "double: " << std::fixed << std::setprecision(1) << doubleValue << std::endl;
-        } else {
-            throw std::invalid_argument("Invalid conversion");
-        }
-    } catch (const std::invalid_argument& e) {
-        std::cout << "double: impossible" << std::endl;
-    }
-
+    convertToActualType(input);
 }
--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -13,12 +13,15 @@
 #include "ScalarConverter.hpp"
 #include <stdlib.h>
 
+
+
 int	main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cout << "Usage: " << argv[0] << " <input>" << std::endl;
+		std::cout << "Usage: " << argv[0] << " <argv[1]>" << std::endl;
 		return (1);
 	}
+
     ScalarConverter::convert(argv[1]);
 }
