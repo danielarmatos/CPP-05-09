3498cdb1c41d692e620c5095e2389af2473d1522
Daniela Alexandra Reis Matos <dreis-ma@student.42porto.com>
2024-02-03 21:55:10 +0000

    C++ templates

--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -16,7 +16,8 @@
 int main(void)
 {
     // Test with int array:
-    Array<int> intArray(5);
+	std::cout << "Test with int array:" << std::endl;
+	Array<int> intArray(5);
 
     for (int i = 0; i < 5; i++) {
         intArray[i] = i * 2;
@@ -25,27 +26,39 @@ int main(void)
     std::cout << std::endl;
 
     // Test with char array:
+	std::cout << "Test with char array:" << std::endl;
     Array<char> charArray(4);
 
     for (int i = 0; i < 4; i++) {
         charArray[i] = 'A' + i;
         std::cout << "Element at index " << i << ": " << charArray[i] << std::endl;
     }
+	std::cout << std::endl;
 
-    // Test with dynamically allocated int
-    int* a = new int();
-    *a = 3;
+	// Test with empty array
+	std::cout << "Test with empty array:" << std::endl;
+	Array<int> emptyArray;
+	try {
+		emptyArray[0] = 1;
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl << std::endl;
+	}
 
-    std::cout << std::endl << "Dynamically allocated int value: " << *a << std::endl;
-    Array<int> intArray2(*a);
-    for (unsigned int i = 0; i < intArray2.size(); i++) {
-        intArray2[i] = i * 2;
-        std::cout << "Element at index " << i << ": " << intArray2[i] << std::endl;
-    }
-    delete a;
-    std::cout << std::endl;
+	// Test with complex type
+	std::cout << "Test with complex type:" << std::endl;
+	Array<int*> complexArray(3);
+	int* a = new int();
+	*a = 3;
+
+	for (int i = 0; i < 3; i++) {
+		complexArray[i] = a;
+		std::cout << "Element at index " << i << ": " << complexArray[i] << std::endl;
+	}
+	delete a;
+	std::cout << std::endl;
 
     // Test with copy constructor
+	std::cout << "Test with copy constructor:" << std::endl;
     Array<int> originalArray(3);
     originalArray[0] = 10;
     originalArray[1] = 20;
@@ -66,6 +79,7 @@ int main(void)
     std::cout << std::endl << std::endl;
 
     // Test with assignment operator
+	std::cout << "Test with assignment operator:" << std::endl;
     Array<double> originalArray2(4);
     originalArray2[0] = 1.1;
     originalArray2[1] = 2.2;
@@ -102,6 +116,7 @@ int main(void)
     std::cout << std::endl << std::endl;
 
     // Test with index out of bounds
+	std::cout << "Test with index out of bounds:" << std::endl;
     try
     {
         std::cout << "Element at index 100: " << charArray[100] << std::endl;
